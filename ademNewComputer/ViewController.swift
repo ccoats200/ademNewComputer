c6e53610c0439542e1d9aaeadbb29aa888d8aa41
ccoats200 <ccoats200@gmail.com>
2019-07-22 19:09:53 -0400

    Initial Commit

---
+++ ademNewComputer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ademNewComputer
+//
+//  Created by Coleman Coats on 7/22/19.
+//  Copyright © 2019 Coleman Coats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
